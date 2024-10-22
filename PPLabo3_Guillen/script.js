1be9995931616c4f971fab4e22e0446abde94569
Ignacio Guillen <69739634+ignacioG00@users.noreply.github.com>
2024-10-21 22:08:15 -0300

    me confundi de archivos, paso este

--- PPLabo3_Guillen/script.js
+++ PPLabo3_Guillen/script.js
@@ -8,6 +8,9 @@ class Persona {
         this.apellido = apellido;
         this.edad = edad;
     }
+    toString() {
+        return `Id: ${this.id} - Nombre: ${this.nombre} - Apellido: ${this.apellido} - Edad: ${this.edad}`; 
+    }
 }
 
 // Clase derivada Futbolista
@@ -20,6 +23,9 @@ class Futbolista extends Persona {
         this.equipo = equipo;
         this.posicion = posicion;
         this.cantGoles = cantGoles;
+    }
+    toString() {
+        return `${super.toString()} - Equipo: ${this.equipo} - Posición: ${this.posicion} - Cantidad de Goles: ${this.cantidadGoles}`;
     }
 }
 
@@ -34,10 +40,29 @@ class Profesional extends Persona {
         this.facultad = facultad;
         this.añoGraduacion = añoGraduacion;
     }
+    toString() {
+        return `${super.toString()} - Título: ${this.titulo} - Facultad: ${this.facultad} - Año de Graduación: ${this.añoGraduacion}`;
+    }
+}
+function parsearArray() {
+    const personas = 
+    `[{"id":1, "nombre":"Marcelo", "apellido":"Luque", "edad":45, "titulo":"Ingeniero", "facultad":"UTN", "añoGraduacion":2002},
+      {"id":2, "nombre":"Ramiro", "apellido":"Escobar", "edad":35, "titulo":"Medico", "facultad":"UBA", "añoGraduacion":20012},
+      {"id":3, "nombre":"Facundo", "apellido":"Cairo", "edad":30, "titulo":"Abogado", "facultad":"UCA", "añoGraduacion":2017},
+      {"id":4, "nombre":"Fernando", "apellido":"Nieto", "edad":18, "equipo":"Independiente", "posicion":"Delantero", "cantidadGoles":7},
+      {"id":5, "nombre":"Manuel", "apellido":"Loza", "edad":20, "equipo":"Racing", "posicion":"Volante", "cantidadGoles":2},
+      {"id":6, "nombre":"Nicolas", "apellido":"Serrano", "edad":23, "equipo":"Boca", "posicion":"Arquero", "cantidadGoles":0}]`;
+
+    return JSON.parse(personas).map((item) => {
+        if (item.titulo !== undefined) {
+            return new Profesional(item.id, item.nombre, item.apellido, item.edad, item.titulo, item.facultad, item.añoGraduacion);
+        } else {
+            return new Futbolista(item.id, item.nombre, item.apellido, item.edad, item.equipo, item.posicion, item.cantidadGoles);
+        }
+    });
 }
 
 let ordenAscendente = true; 
-let personas = [];
 
 function precargarPersonas() {
     const personasPrecargadas = [
